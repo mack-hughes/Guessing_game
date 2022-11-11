17add86e35e1015fa9abb1b035ed202de42852f1
mack hughes <mackhughes251@gmail.com>
2022-11-11 17:31:05 -0300

    New it has a random seed

--- Guessing_game.c
+++ Guessing_game.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
 
+    srand(time(0));
 
     // Declaring the variable
-    int secretvalue = 42;
+    int secretvalue = rand() % 100;
     int guess;
     int play_again = 1;
     int number_of_attempts = 1;
@@ -25,30 +27,34 @@ int main() {
 
 
         if(guess == secretvalue){
-            printf("Congratulations! You nailed it\n");
-            printf("You played on the %d time\n", number_of_attempts);
-            printf("Your punctuation is %.1f\n", punctuation);
+            printf("Congratulations! You nailed it.\n");
+            printf("You played on the %d time.\n", number_of_attempts);
+            printf("Your punctuation is %.1f.\n", punctuation);
             break;
         }
         else{
             printf("You missed!\n");
             
             if(guess > secretvalue){
-                printf("Your guess was bigger than the secret number\n");
+                printf("Your guess was bigger than the secret number.\n");
             }
             else{
-                printf("Your guess was smaller than the secret number\n");
+                printf("Your guess was smaller than the secret number.\n");
             }
             
-            punctuation = punctuation - (double)(guess-secretvalue)/2;
+            punctuation = punctuation - (double)(abs(guess-secretvalue))/2;
 
             if(punctuation <= 0){
-                printf("Your punctuation is already zero. You can not play anymore");
+                printf("Your punctuation is already zero. You can not play anymore, the secret value was %d.\n", secretvalue);
                 break;
             }
             else{
                 printf("Do you wanna play again (1/0)? \n");
                 scanf("%d", &play_again);
+                
+                if(play_again == 0){
+                    printf("the secret value was %d.\n", secretvalue);
+                }
             }
 
         }
